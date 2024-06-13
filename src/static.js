d5cbb258eaaa0605c3b9160c9a074a4d958d41e6
hyunahrho29@gmail.com <hyunahrho29@gmail.com>
2024-06-13 18:06:51 +0900

    modify: setting authorization headers

--- src/static.js
+++ src/static.js
@@ -7,10 +7,12 @@ export const headersNoToken = {
   Accept: "application/json",
 };
 
-export const headersWithToken = {
-  "Access-Control-Allow-Origin": "*",
-  "ngrok-skip-browser-warning": "69420",
-  "Content-Type": "application/json",
-  Accept: "application/json",
-  Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+export const getHeadersWithToken = () => {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "ngrok-skip-browser-warning": "69420",
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
 };
